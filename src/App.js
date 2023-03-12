be5b30136989003b23871ded7a57e458f8e0c273
DEVNET <94292739+DEVNETWORLD@users.noreply.github.com>
2023-03-13 02:25:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./styles.css";
+import React, { useState } from "react";
+var directorList= {
+  "Steven Spielberg" : "Scheindler's List",
+  "James Cameron" : "Titanic",
+  "Quentein Tarantino": "Pulp Fiction",
+  "Cristopher Nolan": "Prestige"
+
+};
+var currentList= Object.keys(directorList);
+
 
 export default function App() {
+  const [movies, setmovies]= useState("");
+   function clickdirectorHanler(movie){
+var movies = directorList[movie];
+setmovies(movies);
+   }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+      <h1>Rating Best Directors works</h1>
+      <h3>Mine all time favourite</h3>
+      {
+      currentList.map(function (movie){      return (
+        <span
+          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+          key={movie}
+          onClick={() => clickdirectorHanler(movie)}
+        >
+          {movie}
+        </span>
+      );
+    })}
+    <h2 style={{ font: "bold" }}>{movies}</h2>
+      </div>
+  
   );
 }
