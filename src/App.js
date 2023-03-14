bdff5107e3064162e390e620d0b60516aa96b9f0
DEVNET <94292739+DEVNETWORLD@users.noreply.github.com>
2023-03-15 02:46:46 +0530

    button design

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
 import "./styles.css";
 import React, { useState } from "react";
-var directorList= {
-  "Steven Spielberg" : "Scheindler's List",
-  "James Cameron" : "Titanic",
+var directorList = {
+  "Steven Spielberg": "Scheindler's List",
+  "James Cameron": "Titanic",
   "Quentein Tarantino": "Pulp Fiction",
   "Cristopher Nolan": "Prestige"
-
 };
-var currentList= Object.keys(directorList);
-
+var currentList = Object.keys(directorList);
 
 export default function App() {
-  const [movies, setmovies]= useState("");
-   function clickdirectorHanler(movie){
-var movies = directorList[movie];
-setmovies(movies);
-   }
+  const [movies, setmovies] = useState("");
+  function clickdirectorHanler(movie) {
+    var movies = directorList[movie];
+    setmovies(movies);
+  }
   return (
     <div className="App">
       <h1>Rating Best Directors works</h1>
-      <h3>Mine all time favourite</h3>
-      {
-      currentList.map(function (movie){      return (
-        <span
-          style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
-          key={movie}
-          onClick={() => clickdirectorHanler(movie)}
-        >
-          {movie}
-        </span>
-      );
-    })}
-    <h2 style={{ font: "bold" }}>{movies}</h2>
-      </div>
-  
+      <p>Mine all time favourite</p>
+      {currentList.map(function (movie) {
+        return (
+          <span
+            style={{
+              border: "1px solid var",
+              backgroundColor: "yellow",
+              fontSize: "1rem",
+              paddingRight: "0.5rem",
+              paddingLeft: "0.5rem",
+              paddingTop: "1rem",
+              paddingBottom: "1rem",
+
+              cursor: "pointer"
+            }}
+            key={movie}
+            onClick={() => clickdirectorHanler(movie)}
+          >
+            {movie}
+          </span>
+        );
+      })}
+      <h2 style={{ font: "bold" }}>{movies}</h2>
+    </div>
   );
 }
